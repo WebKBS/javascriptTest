9483e58996b4b9d4234e89b35ea764bab1e6311c
강범수 <kbs21c1987@gmail.com>
2023-07-12 19:00:46 +0900

    module

---
+++ moduleTest/module.js
@@ -0,0 +1,27 @@
+export const alert1 = () => {
+  alert('TEST!!!!!!');
+};
+
+export const alert2 = () => {
+  alert('TEST!!!!!!');
+};
+
+const alert3 = () => {
+  alert('TEST!!!!!!');
+};
+
+const alert4 = () => {
+  alert('TEST!!!!!!');
+};
+
+const alert5 = () => {
+  alert('TEST!!!!!!');
+};
+
+const alert6 = () => {
+  alert('TEST!!!!!!');
+};
+
+const alert7 = () => {
+  alert('TEST!!!!!!');
+};
